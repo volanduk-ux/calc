557a05020c0240cbe18b7fe09e479656f806b95d
volanduk-ux <volanduk@gmail.com>
2025-08-27 00:05:46 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'magic-calc-v3';
+const CACHE = 'magic-calc-v4';
 const ASSETS = ['.', './index.html', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
